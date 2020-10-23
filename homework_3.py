312affaf12e7674988db18235dbdf03da3b6749a
mihwa-kim <64059691+mihwa-kim@users.noreply.github.com>
2020-10-23 16:33:13 +0900

    Homework4
    다 맞게 작성한거 같은데.. 가동이 안되네요...

--- homework_3.py
+++ homework_3.py
@@ -1,5 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
+from pymongo import MongoClient
+
+client = MongoClient('localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
+db = client.dbsparta
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
